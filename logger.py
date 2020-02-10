6447e46dc8635b0a164ebec341573a7b0d5ce68a
ntt123 <ntt123@earth>
2020-02-10 12:42:33 +0800

    remove tensorflow and tensorboardX; able to run on CPU only environments

--- logger.py
+++ logger.py
@@ -1,12 +1,18 @@
 import random
 import torch
-from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 from plotting_utils import plot_alignment_to_numpy, plot_spectrogram_to_numpy
 from plotting_utils import plot_gate_outputs_to_numpy
 
 
 class Tacotron2Logger(SummaryWriter):
     def __init__(self, logdir):
+        ## logdir + current time + hostname
+        import socket, os
+        from datetime import datetime
+        current_time = datetime.now().strftime('%b%d_%H-%M-%S')
+        logdir = os.path.join(logdir, current_time + '_' + socket.gethostname())
+
         super(Tacotron2Logger, self).__init__(logdir)
 
     def log_training(self, reduced_loss, grad_norm, learning_rate, duration,
@@ -27,22 +33,22 @@ class Tacotron2Logger(SummaryWriter):
             self.add_histogram(tag, value.data.cpu().numpy(), iteration)
 
         # plot alignment, mel target and predicted, gate target and predicted
-        idx = random.randint(0, alignments.size(0) - 1)
+        idx = 0 # random.randint(0, alignments.size(0) - 1)
         self.add_image(
             "alignment",
             plot_alignment_to_numpy(alignments[idx].data.cpu().numpy().T),
-            iteration)
+            iteration, dataformats="HWC")
         self.add_image(
             "mel_target",
             plot_spectrogram_to_numpy(mel_targets[idx].data.cpu().numpy()),
-            iteration)
+            iteration, dataformats="HWC")
         self.add_image(
             "mel_predicted",
             plot_spectrogram_to_numpy(mel_outputs[idx].data.cpu().numpy()),
-            iteration)
+            iteration, dataformats="HWC")
         self.add_image(
             "gate",
             plot_gate_outputs_to_numpy(
                 gate_targets[idx].data.cpu().numpy(),
                 torch.sigmoid(gate_outputs[idx]).data.cpu().numpy()),
-            iteration)
+            iteration, dataformats="HWC")
--- utils.py
+++ utils.py
@@ -5,8 +5,9 @@ import torch
 
 def get_mask_from_lengths(lengths):
     max_len = torch.max(lengths).item()
-    ids = torch.arange(0, max_len, out=torch.cuda.LongTensor(max_len))
-    mask = (ids < lengths.unsqueeze(1)).byte()
+    ids = torch.arange(0, max_len, dtype=torch.long)
+    if torch.cuda.is_available(): ids = ids.cuda()
+    mask = (ids < lengths.unsqueeze(1))
     return mask
 
 
